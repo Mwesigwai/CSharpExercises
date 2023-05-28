05cbeec844a7953beeb22ea0550dec1f3bc5a3bb
Mwesigwai <mwesigwai433@gmail.com>
2023-05-28 19:35:45 +0300

    Added a for loop to try reading the string in a reversed order but it does not include the first character and im trying to figure out how to fix it    plus i added some continuity to the app by adding a do while loop

--- Exercise3.cs
+++ Exercise3.cs
@@ -6,12 +6,24 @@ namespace Exercise
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("This program will revert the order of the word you enter");
-            Console.WriteLine("Enter a word and see how it is reverted");
-            string word = Console.ReadLine();
-            char[] wordAsChars = word.ToCharArray();
-            //Console.WriteLine(wordAsChars[1]);
-
+            string choice;
+            do
+            {
+                Console.WriteLine("This program will revert the order of the word you enter");
+                Console.WriteLine("Enter a word and see how it is reverted");
+                string word = Console.ReadLine();
+                char[] wordAsChars = word.ToCharArray();
+                //Console.WriteLine(wordAsChars[1]);
+                for (int i = word.Length - 1; i > 0; i--)
+                {
+                    Console.Write(wordAsChars[i]);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Do you want to start again? ");
+                Console.WriteLine("Type yes to restart");
+                choice = Console.ReadLine();
+            } while (choice == "yes");
         }
     }
 }
