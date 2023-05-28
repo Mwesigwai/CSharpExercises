b8433cf483a3bf6671254697f51c5cc8b2f92dd1
Mwesigwai <mwesigwai433@gmail.com>
2023-05-28 19:58:58 +0300

    First commit of the exercise 4.  i commited it at this level because when i tried to test it, i didnot see any number with a fizz buzz phrase. MayBe because there is no number from  1 - 100 that is divisable by both 3 and five? but they are there.

---
+++ Exercise4.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Exercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, here is a fizz buzz game in counting numbers");
+            Console.WriteLine("Its going to add fizz to numbers divisible by 3");
+            Console.WriteLine("fizz buzz for the numbers divisible by both 3 and 5");
+            Console.WriteLine("And just a number for the numbers that are not divisible by any");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                //Console.WriteLine(i);
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " buzz");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " fizz buzz");
+                }
+
+
+            }
+        }
+    }
+}
+
