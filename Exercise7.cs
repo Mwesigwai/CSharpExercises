6e7d0ee4a7ba629c0122a13d4c3d33ff3d6cb5cc
Mwesigwai <mwesigwai433@gmail.com>
2023-05-31 21:58:18 +0300

    Added functionality to generate random numbers and for loop to help count the number of trial times

--- Exercise7.cs
+++ Exercise7.cs
@@ -1,59 +1,63 @@
 using System;
-using System.Linq;
 
 namespace CS
 {
-    class MainClass
+    class Exercise7
     {
-        public static bool feedIsTrue;
         public static int result;
         public static void Main(string[] args)
         {
-            //bool shouldContinue;
+
             int usersGuess;
             bool userIsTrue;
             Console.WriteLine("NUmber guesing program");
+
+            Random myNumbers = new Random();
+            int randomNumbers = myNumbers.Next(1, 10);
 
             do
             {
                 Console.WriteLine("Enter numbers from 1 upto 10 to see if you win");
                 string userINput = Console.ReadLine();
-                userIsTrue = int.TryParse(userINput, out  result)&& result >= 1 && result <= 10; ;
+                userIsTrue = int.TryParse(userINput, out result) && result >= 1 && result <= 10; ;
 
                 if (userIsTrue)
                 {
                     usersGuess = result;
-                    //TestLimit(result);
-                    //shouldContinue = usersGuess >= 1 && usersGuess <= 10;
-                   /* do
-                    {
-                        Console.WriteLine("Number must start from 1 to 10");
-                        if (shouldContinue)
-                        {
 
+                    for (int i = 0; i < 2; i++)
+                    {
+                        if (usersGuess != randomNumbers)
+                        {
+                            Console.WriteLine("try again");
+                            userINput = Console.ReadLine();
+                            //userIsTrue = int.TryParse(userINput, out result) && result >= 1 && result <= 10; ;
                         }
                     }
-                    while (!shouldContinue);*/
+
+                    if (result == randomNumbers)
+                        Console.WriteLine("You won!!!!!!!!!!!!!!!");
+                    else
+                        Console.WriteLine("You lose");
+
+                    //TestLimit(result);
+                    //shouldContinue = usersGuess >= 1 && usersGuess <= 10;
+                    /* do
+                     {
+                         Console.WriteLine("Number must start from 1 to 10");
+                         if (shouldContinue)
+                         {
+
+                         }
+                     }
+                     while (!shouldContinue)*/
+
                 }
             } while (!userIsTrue);
-        }
 
-        /*public static void TestLimit(int feed)
-        {
-            feedIsTrue = feed >= 1 && feed <= 10;
-            do
-            {
-                if (!feedIsTrue)
-                {
-                    Console.WriteLine("Number must start from 1 to 10");
-                    if (feedIsTrue)
-                    {
-
-                    }
-                }
-            } while (!feedIsTrue);*/
         }
     }
+}
 
 
 
