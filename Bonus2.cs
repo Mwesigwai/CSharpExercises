f57ff9055fe4534d85a23d551fbd69732cf41a0f
Mwesigwai <mwesigwai433@gmail.com>
2023-06-01 19:16:05 +0300

    resolved all the issues

--- Bonus2.cs
+++ Bonus2.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Exercise
 {
     class MainClass
@@ -5,13 +7,16 @@ namespace Exercise
         public static int number;
         public static void Main(string[] args)
         {
-            int countingNumbers;
+            string usersChoice;
+            do 
+            { 
+                int countingNumbers;
 
-            bool userInput;
-            Console.WriteLine("This program prints a multiplication table of the number you enter upto ten numbers");
-            Console.WriteLine("Enter a number");
-            do
-            {
+                bool userInput;
+                Console.WriteLine("This program prints a multiplication table of the number you enter upto ten numbers");
+                Console.WriteLine("Enter a number");
+                do
+                {
                 Console.WriteLine("Make sure it is a real number");
                 string numString = Console.ReadLine();
                 userInput = int.TryParse(numString, out int result);
@@ -26,14 +31,18 @@ namespace Exercise
                 }
             } while (!userInput);
 
-            for (countingNumbers  = 1; countingNumbers <= 10; countingNumbers++)
+            for (countingNumbers = 1; countingNumbers <= 10; countingNumbers++)
             {
-                Console.WriteLine(number + "*" + countingNumbers = number * countingNumbers);
+                Console.WriteLine(number + " * " + countingNumbers + " = " + (number * countingNumbers));
             }
-
+                Console.WriteLine("Repeat? y for repeat");                                   
+                usersChoice = Console.ReadLine();
+        
+        }while (usersChoice .Equals( "y" ) || usersChoice.Equals("Y")) ;
 
             //Console.WriteLine("The multiplication table for {0} is: ", number);
         }
     }
 }
+
 
